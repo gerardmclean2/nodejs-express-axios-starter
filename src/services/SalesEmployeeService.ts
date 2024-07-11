aaaa4c0a6eefcc1c87201cab9454953514dd3a45
erinmcj <erinmcjannet@gmail.com>
2024-07-11 15:03:59 +0100

    frontend for get all sales employees

---
+++ src/services/SalesEmployeeService.ts
@@ -0,0 +1,12 @@
+import axios, { AxiosResponse } from "axios";
+import { SalesEmployeeResponse } from "../models/SalesEmployeeResponse";
+
+export const getSalesEmployees = async (): Promise<SalesEmployeeResponse[]> => {
+    try {
+        const response: AxiosResponse = await axios.get("http://localhost:8080/api/sales-employee");
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        throw new Error('Failed to get Sales Employees');
+    }
+}
