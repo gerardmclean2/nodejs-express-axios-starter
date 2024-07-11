a5e11be7e62ff9653fa28ac7eebb5a0ba2b46d57
Jamie McConnell <jamie.mcconnell@kainos.com>
2024-07-11 10:37:17 +0100

    Created delivery employee service

---
+++ src/services/DeliveryEmployee.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosResponse } from "axios";
+import { DeliveryEmployeeRequest } from "../models/DeliveryEmployeeRequest";
+
+export const createDeliveryEmployee = async (deliveryEmployee: DeliveryEmployeeRequest): Promise<Number> => {
+    try{
+        const response: AxiosResponse = await axios.post("http://localhost:8080/api/delivery-employee", deliveryEmployee);
+
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        throw new Error(e.response.data);
+    }
+}
